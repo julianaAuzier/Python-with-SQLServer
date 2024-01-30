e25aa61a7eb1b0517b29a5fd7f27cf3d8f8ca59b
Juliana Auzier Seixas Feio <juliana.auzier.s@gmail.com>
2024-01-30 13:43:23 -0300

    main

---
+++ main.py
@@ -0,0 +1,26 @@
+import pyodbc
+import pandas as pd
+pd.set_option('display.max_columns',1000)
+
+data_conx = (
+    'Driver={SQL Server};'
+    'Server=DESKTOP-UJJVFD1\SQLEXPRESS;'
+    'Database=AdventureWorks2017;'
+    'Trusted_connection=yes;'
+)
+
+conx = pyodbc.connect(data_conx)
+#print('connected!')
+
+cursor = conx.cursor()
+query = """select * from Person.Address where PostalCode like 98011"""
+#print(cursor.execute(query).fetchall())
+
+cursor.execute(query)
+
+AddressID, AddressLine1, AddressLine2, City, StateProvinceID, PostalCode, SpatialLocation, rowguid, ModifiedDate = zip(*cursor.fetchall())
+
+df = pd.concat([pd.DataFrame(k) for k in [AddressID, AddressLine1, AddressLine2, City, StateProvinceID, PostalCode, SpatialLocation, rowguid, ModifiedDate]], axis=1, ignore_index=True)
+df.columns = [k for k in 'AddressID, AddressLine1, AddressLine2, City, StateProvinceID, PostalCode, SpatialLocation, rowguid, ModifiedDate'.split(', ')]
+print(df)
+
